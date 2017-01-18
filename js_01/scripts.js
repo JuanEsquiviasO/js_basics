ed837ce690960dde765e595291fe0da0f0f87635
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-01-18 13:29:10 -0500

    fundamentals

---
+++ js_01/scripts.js
@@ -0,0 +1,30 @@
+// alert('running javascript!!');
+
+(function () {
+	function run() {
+		var jump = 'Jumper';
+		alert(jump);
+	}
+
+	// run();
+
+	var myObject = {
+		name: 'movil'
+	}
+
+	var drink = function() {
+		console.log('Coke');
+	};
+
+	var name = 'Fokker';
+
+	// alert(name);
+
+	var Skull;
+
+	Skull = 'pilot';
+
+	var a = 1;
+	alert(++a);
+	alert(a);
+}());
