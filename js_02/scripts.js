9ab1253d1271b57d4f7aa16a87448fee8dd5a9e4
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-01-24 13:58:53 -0500

    arrays, loops and more

---
+++ js_02/scripts.js
@@ -0,0 +1,70 @@
+// condicionales
+
+// var edad = 22;
+
+// if (edad > 0 && edad < 10) {
+// 	console.log('Eres un infante');
+// } else if (edad >= 11 && edad < 20) {
+// 	console.log('Eres un adolescente');
+// } else if (edad > 21) {
+// 	console.log('Eres un adulto');
+// }
+
+// operadores
+// &&
+// ||
+// !
+
+// switch
+// var fecha = new Date();
+// var dia = fecha.getDay();
+
+// switch(dia){
+// 	case 0:
+// 		console.log('Es domingo');
+// 		break;
+// 	case 1:
+// 		console.log('Es lunes');
+// 		break;
+// 	case 2:
+// 		console.log('Es martes');
+// 		console.log('Es verano');
+// 		break;
+// 	case 3:
+// 		console.log('Es miercoles');
+// 		break;
+// 	default:
+// 		console.log('No sé que día es..');
+// 		break;
+// }
+
+//ternarios
+
+// false ? console.log('es verdadero') : console.log('es falso');
+
+// Loops
+
+// var array = ['Fokker', 'Hunter', 'Sterling', 'Exedor', 'Global'];
+// var arrayLength = array.length;
+
+// for (i = 0; i < arrayLength; i++) {
+// 	document.write(array[i] + "<br>");
+// }
+
+// var i = 0;
+// while(i < arrayLength){
+// 	document.write(array[i] + "<br>");
+// 	i++;
+// }
+
+// var i = 0;
+// do {
+// 	document.write(array[i] + "<br>");
+// 	i++;
+// } while(i < arrayLength);
+//
+
+// var i;
+// for (i in array){
+// 	document.write(array[i] + "<br>");
+// }
